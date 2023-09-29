188d43f73c328a140b1f1016e97376396d80b705
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2023-09-29 12:20:31 +0200

    Formatting

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -2,11 +2,8 @@ using Backend.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices(builder.Configuration);
