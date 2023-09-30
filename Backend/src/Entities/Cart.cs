5451fcd5294b59aa2bd934612b20175ee04be366
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2023-09-30 17:34:23 +0200

    format

--- Backend/src/Entities/Cart.cs
+++ Backend/src/Entities/Cart.cs
@@ -10,6 +10,6 @@ namespace Backend.Entities
         public int Id { get; set; }
         public required ICollection<Shoes> Shoes { get; set; }
         public required AppUser User { get; set; }
-    public int UserId { get; set; }
-}
+        public int UserId { get; set; }
+    }
 }
