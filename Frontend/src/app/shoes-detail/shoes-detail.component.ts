26f5e79881fc7fa128b13c74ee3700e9ce1511e3
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2023-10-02 14:18:18 +0200

    client side sizes

--- Frontend/src/app/shoes-detail/shoes-detail.component.ts
+++ Frontend/src/app/shoes-detail/shoes-detail.component.ts
@@ -5,21 +5,37 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { ActivatedRoute } from '@angular/router';
 import { ShoesSeviceService } from '../_services/shoes-sevice.service';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  NgForm,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-shoes-detail',
   standalone: true,
-  imports: [NgFor, CommonModule, TabsModule, GalleryModule],
+  imports: [
+    NgFor,
+    CommonModule,
+    TabsModule,
+    GalleryModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './shoes-detail.component.html',
   styleUrls: ['./shoes-detail.component.css'],
 })
 export class ShoesDetailComponent {
   shoes: Shoes | undefined;
   images: GalleryItem[] = [];
+  sizeId: number = 0;
+  checkoutForm: any = {};
 
   constructor(
     private shoesService: ShoesSeviceService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -40,5 +56,8 @@ export class ShoesDetailComponent {
     for (const photo of this.shoes.photos) {
       this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
     }
+  }
+  onSubmit() {
+    console.log(this.sizeId);
   }
 }
